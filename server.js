55712dab946c099f9ae1030172e5a87f997f91ea
RosaRamosLuisAntonio <90283694+RosaRamosLuisAntonio@users.noreply.github.com>
2025-05-25 22:34:12 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-require('dotenv').config();
+require('dotenv').config(); 
+
+const express = require('express');
 const app = require('./src/app');
 const connectDB = require('./database/db');
 
+const helmet = require('helmet');
+
+app.use(helmet()); // Seguridad HTTP headers
+
 const PORT = process.env.PORT || 3000;
 
-connectDB();
+connectDB(); // Conexión a MongoDB
 
 app.listen(PORT, () => {
   console.log(`IDS escuchando en puerto ${PORT}`);
